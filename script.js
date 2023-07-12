d3fc09be9a6bf9d4a15954fc856a39d33d1eca36
Khthonian <anansicorp@tuta.io>
2023-07-12 13:02:29 +0100

    Add variable grid sizing
    - Variable grid sizing fully implemented
    - Default, min, and max values are included in variable sizing
    - General CSS improvements
    - Icons implemented for buttons

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // Get the container grid
 const container = document.querySelector(".container");
 
-// Get the grid size
-let gridSize = 10;
+// Get the control inputs
+const resetButton = document.querySelector(".reset");
+const gridSizeValue = document.querySelector(".grid-size");
+const applyButton = document.querySelector(".apply");
 
-function makeGrid() {
+// Get the grid size
+let gridSizeInput = document.querySelector("input");
+let gridSize = 16;
+
+
+function makeGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         for(let j = 0; j < gridSize; j++) {
             const cell = document.createElement("div");
@@ -20,6 +27,25 @@ function makeGrid() {
     }
 }
 
+// Change the grid size value
+gridSizeInput.addEventListener("input", function (e) {
+    gridSize = e.target.value;
+    gridSizeValue.textContent = `${gridSize} x ${gridSize}`;
+});
 
+// Enable grid resetting
+function resize() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    makeGrid(gridSize);
+}
 
-makeGrid();
+resetButton.addEventListener("click", function () {
+    gridSize = 16;
+    resize();
+});
+
+applyButton.addEventListener("click", resize);
+
+makeGrid(gridSize);
